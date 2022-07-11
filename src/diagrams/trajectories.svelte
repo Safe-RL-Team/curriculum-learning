7442aaf751880aaf4c36ea32c8cbce36f628e627
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2022-07-11 15:34:14 +0200

    add example visualization

---
+++ src/diagrams/trajectories.svelte
@@ -0,0 +1,30 @@
+<script>
+  const len = 5;
+  let count = 0;
+
+  // the `$:` means 're-run whenever these values change'
+  $: doubled = count * 2;
+  $: quadrupled = doubled * 2;
+
+  function handleClick() {
+    count += 1;
+    count %= len
+  }
+</script>
+
+Trajectory {count + 1}
+<button on:click={handleClick}>Next</button>
+<!-- <embed
+  src="images/trajectories{count}.pdf"
+  type="application/pdf"
+  frameBorder="0"
+  scrolling="no"
+  height="600px"
+  width="600px"
+/> -->
+<img 
+  src="images/trajectories{count}.svg"
+  alt="TODO"
+  height="300px"
+  width="300px"
+/>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import * as _unused from "raw-loader!./index.ejs";
 // TODO: disable before publishing
 
 import Example from "./diagrams/svelte-example.svelte";
+import Trajectories from "./diagrams/trajectories.svelte";
 
 // lazily initialize any diagram below the fold. E.G:
 const exampleTag = document.getElementById("svelte-example-dfigure");
@@ -10,4 +11,12 @@ let example;
 exampleTag.addEventListener("ready", () => {
 	const target = exampleTag.querySelector("#svelte-example-target");
 	example = new Example({ target });
+});
+
+
+const trajectoriesTag = document.getElementById("trajectories-visualization");
+let trajectories;
+trajectoriesTag.addEventListener("ready", () => {
+	const target = trajectoriesTag.querySelector("#trajectories-visualization-target");
+	trajectories = new Trajectories({ target });
 });
