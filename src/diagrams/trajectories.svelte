57066c694de1a3e298254b428fb8a049223b8308
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2022-07-12 14:15:29 +0200

    add some headers

--- src/diagrams/trajectories.svelte
+++ src/diagrams/trajectories.svelte
@@ -2,10 +2,6 @@
   const len = 5;
   let count = 0;
 
-  // the `$:` means 're-run whenever these values change'
-  $: doubled = count * 2;
-  $: quadrupled = doubled * 2;
-
   function input(x) {
     console.log(x.target.value)
     count = parseInt(x.target.value)
@@ -13,17 +9,9 @@
 </script>
 
 <div style="width:300px; margin:auto; text-align:center;">
-  <p>Trajectory {count + 1}</p>
+  <p>Trajectory after curriculum step {count + 1}</p>
   <input type="range" min="0" max="{len}" value="0" on:input={input}>
 </div>
-<!-- <embed
-  src="images/trajectories{count}.pdf"
-  type="application/pdf"
-  frameBorder="0"
-  scrolling="no"
-  height="600px"
-  width="600px"
-/> -->
 <img 
   src="images/trajectories{count}.svg"
   alt="TODO"
