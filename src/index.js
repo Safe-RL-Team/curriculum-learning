5a57437a621c0f49036aa986da300f3caa21d974
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2022-08-06 11:47:38 +0200

    remove example content

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@
 import * as _unused from "raw-loader!./index.ejs";
 // TODO: disable before publishing
 
-import Example from "./diagrams/svelte-example.svelte";
 import Trajectories from "./diagrams/trajectories.svelte";
 import Back from "./diagrams/back.svelte"
 import Incremental from "./diagrams/incremental.svelte"
 
 // lazily initialize any diagram below the fold. E.G:
-const exampleTag = document.getElementById("svelte-example-dfigure");
-let example;
-exampleTag.addEventListener("ready", () => {
-	const target = exampleTag.querySelector("#svelte-example-target");
-	example = new Example({ target });
-});
-
 
 const trajectoriesTag = document.getElementById("trajectories-visualization");
 let trajectories;
