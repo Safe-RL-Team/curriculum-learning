59260d42ad61574e46c7f4272b56c6195533842b
Marvin Sextro <marvin.sextro@gmail.com>
2022-08-11 04:00:32 +0200

    Fix confetti

--- src/diagrams/trajectories.svelte
+++ src/diagrams/trajectories.svelte
@@ -12,13 +12,13 @@
 </script>
 
 <div style="display: flex; flex-direction: column; align-items: center; padding-top: 1em">
-  <img
-    src="images/trajectories{count}.svg"
-    alt="Interactive plot displaying the trajectories of the RL agent on the map"
-    height="300px"
-    width="300px"
-  />
-  <div style="margin:auto; text-align:center;">
+  <div style="max-width:300px; width:100%;">
+    <img
+      src="images/trajectories{count}.svg"
+      alt="Interactive plot displaying the trajectories of the RL agent on the map"
+    />
+  </div>
+  <div style="margin: auto; text-align:center;">
     <p>Trajectory after curriculum step {count + 1}</p>
     <input type="range" min="0" max="{len}" value="0" on:input={input}>
     <p style="height: 1em; transition: 1s opacity; {hasInteracted && 'opacity: 0;'}">Move the slider to change the step!</p>
